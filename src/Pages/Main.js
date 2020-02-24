5046c5a493b92b26a90f6420570fe5a3104cce90
mrhow99 <wonyoungchoiseoul@gmail.com>
2020-02-24 14:15:43 +0900

    first commit

---
+++ src/Pages/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../Styles/Main.scss'
+
+export default class Home extends React.Component {
+  render() {
+    return(
+      <div>
+
+      </div>
+    )
+  }
+}
+
