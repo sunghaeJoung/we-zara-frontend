fec3c31b2190647f964ffad9de03dc806f5af5bc
mrhow99 <wonyoungchoiseoul@gmail.com>
2020-02-25 17:48:50 +0900

    commits

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -15,23 +15,67 @@ const data = [
       1: {
         title: "new in",
         subtitle: {
-          1: "psfaionegobogpbbheihfoioins"
+          1: "Explore week's latest menswear pieces of the",
+          2: "season curated for you.",
+          3: "Spring Summer Man Collection"
+        }
+      },
+      2: {
+        title: "knitwear",
+        subtitle: {
+          1: "Spring pieces designed with style and function in mind"
+        }
+      },
+      3: {
+        title: "collection",
+        subtitle: {
+          1: "Discover this season’s new collection built around",
+          2: "staple pieces and trend-focused items.",
+          3: "Men’s Spring Summer Collection"
+        }
+      },
+      4: {
+        title: "show & bags",
+        subtitle: {
+          1: "Explore the new collection of Shoes & Bags",
+          2: "Spring Summer Man Collection"
         }
       }
     }
   },
   {
     bg: {
-      1: "url(https://static.zara.net/photos//mkt/spots/ss20-north-crossover-woman-basic/subhome-xmedia//landscape_0.jpg) center/cover",
-      2: "url(https://static.zara.net/photos//mkt/spots/ss20-north-awakening-woman-basic/subhome-xmedia-08-1//landscape_0.jpg) center/cover",
-      3: "url(https://static.zara.net/photos//mkt/spots/ss20-north-collection-woman/subhome-xmedia-08-2//landscape_0.jpg) center/cover",
+      1: "url(https://static.zara.net/photos//mkt/spots/ss20-north-join-life-woman/subhome-xmedia//landscape_0.jpg?ts=1582547884469) center/cover",
+      3: "url(https://static.zara.net/photos//mkt/spots/ss20-north-leather-woman-basic/subhome-xmedia-collection//landscape_0.jpg) center/cover",
       4: "url(https://static.zara.net/photos//mkt/spots/ss20-north-shoes-and-bags-woman/subhome-xmedia//landscape_0.jpg) center/cover"
     },
     text: {
       1: {
-        title: "crossover",
+        title: "join life ss20",
         subtitle: {
-          1: "No fashion dictates, no borders is the essence of 2020 allure."
+          1: "Working towards sustainability"
+        }
+      },
+      2: {
+        title: "new in",
+        subtitle: {
+          1: ""
+        }
+      },
+      3: {
+        title: "collection",
+        subtitle: {
+          1: "Discover this week the latest pieces from our latest collection",
+          2: "Spring Summer 2020 Woman",
+          3: "Selected pieces with new pictures curated for you."
+        }
+      },
+      4: {
+        title: "shoes & bags",
+        subtitle: {
+          1: "Explore the new collection of Shoes & Bags",
+          2: "Spring Summer 2020 Woman",
+          3: "Selected pieces with new pictures curated for you"
         }
       }
     }
@@ -48,8 +92,33 @@ const data = [
       1: {
         title: "new in girl",
         subtitle: {
-          1: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis, possimus.",
-          2: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis, possimus."
+          1: "Discover our new denim collection.",
+          2: "Welcome Spring with the latest black",
+          3: "and white denim trends."
+        }
+      },
+      2: {
+        title: "finding neon",
+        subtitle: {
+          1: "Discover our new season’s trend for kids.",
+          2: "Welcome Spring with natural colors embraced",
+          3: "by bright neons."
+        }
+      },
+      3: {
+        title: "collection",
+        subtitle: {
+          1: "Discover this week the latest pieces from our latest collection",
+          2: "Spring Summer 2020 Kids",
+          3: "Selected pieces with new pictures curated for you"
+        }
+      },
+      4: {
+        title: "shoes & bags",
+        subtitle: {
+          1: "Explore the new collection of Shoes & Bags",
+          2: "Spring Summer 2020 Kids",
+          3: "Selected pieces with new pictures curated for you"
         }
       }
     }
@@ -80,6 +149,8 @@ class Slider extends Component {
       "click"
     ).map(() => 1);
 
+    const downBtnClick$ = Rx.Observable.fromEvent(this.refs[""]).map(() => -1);
+
     // stream of mousewheel
     const buttonClicks$ = Rx.Observable.fromEvent()
       .merge(prevBtnClick$, nextBtnClick$)
@@ -100,6 +171,19 @@ class Slider extends Component {
   }
 
   render() {
+    let prev;
+    let next;
+    if (this.state.sliderIndex === 0) {
+      prev = "";
+      next = "WOMAN >";
+    } else if (this.state.sliderIndex === 1) {
+      prev = "MAN <";
+      next = "> KID ";
+    } else {
+      prev = "WOMAN <";
+      next = "";
+    }
+
     const transition = this.state.sliderIndex * -100;
     const style = {
       width: data.length * 100 + "vw",
@@ -108,49 +192,66 @@ class Slider extends Component {
     };
 
     const slides = data.map(item => (
+
       <div className="smooth">
         <section className="main__section" style={{ background: item.bg[1] }}>
           <div className="text-container">
             <div className="text-container__title">{item.text[1].title}</div>
             <div className="text-container__subtitle">
+
               {item.text[1].subtitle[1]}
               <br />
               {item.text[1].subtitle[2]}
+              <br />
+              {item.text[1].subtitle[3]}
             </div>
             <div className="text-container__btn">view</div>
           </div>
+
         </section>
         <section className="main__section" style={{ background: item.bg[2] }}>
+          {this.state.sliderIndex === 1 && (
+            <video loop playsInline autoPlay muted className="video">
+              <source
+                src="https://static.zara.net/video///mkt/2020/2/ss20-north-leather-woman-basic/video/w/2560//portrait/video_0.mp4?ts=1582547763449"
+                type="video/mp4"
+              />
+            </video>
+          )}
           <div className="text-container">
-            <div className="text-container__title">knitwear Collection</div>
+            <div className="text-container__title">{item.text[2].title}</div>
             <div className="text-container__subtitle">
-              Knit is something that you should consider buying
+              {item.text[2].subtitle[1]}
+              <br />
+              {item.text[2].subtitle[2]}
+              <br />
+              {item.text[2].subtitle[3]}
             </div>
             <div className="text-container__btn">view</div>
           </div>
         </section>
         <section className="main__section" style={{ background: item.bg[3] }}>
           <div className="text-container">
-            <div className="text-container__title">Collection</div>
+            <div className="text-container__title">{item.text[3].title}</div>
             <div className="text-container__subtitle">
-              Discover this week the latest pieces from our latest collection
+              {item.text[3].subtitle[1]}
               <br />
-              Spring Summer 2020 Woman
+              {item.text[3].subtitle[2]}
               <br />
-              Selected pieces with new pictures curated for you.
+              {item.text[3].subtitle[3]}
             </div>
             <div className="text-container__btn">view</div>
           </div>
         </section>
         <section className="main__section" style={{ background: item.bg[4] }}>
           <div className="text-container">
-            <div className="text-container__title">Shoes & Bags</div>
+            <div className="text-container__title">{item.text[4].title}</div>
             <div className="text-container__subtitle">
-              Discover this week the latest pieces from our latest collection
+              {item.text[4].subtitle[1]}
               <br />
-              Spring Summer 2020 Woman
+              {item.text[4].subtitle[2]}
               <br />
-              Selected pieces with new pictures curated for you.
+              {item.text[4].subtitle[3]}
             </div>
             <div className="text-container__btn">view</div>
           </div>
@@ -178,19 +279,6 @@ class Slider extends Component {
         </section>
       </div>
     ));
-
-    let prev;
-    let next;
-    if (this.state.sliderIndex === 0) {
-      prev = "";
-      next = "WOMAN >";
-    } else if (this.state.sliderIndex === 1) {
-      prev = "MAN <";
-      next = "> KID ";
-    } else {
-      prev = "WOMAN <";
-      next = "";
-    }
 
     return (
       <main className="main">
