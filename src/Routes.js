37bebce8e813b5db20c686ab74a9bffae9a1f85b
tac0de <wonyoungchoiseoul@gmail.com>
2020-03-02 16:04:13 +0900

    commented signup

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import SignUp from './Pages/SignUp/SignUp';
+// import SignUp from './Pages/SignUp/SignUp';
 import Login from './Pages/Login/Login';
 import Main from './Pages/Main/Main.js';
 import ProductDetail from './Pages/ProductDetail/ProductDetail';
@@ -11,7 +11,7 @@ class Routes extends React.Component {
     return (
       <Router>
         <Switch>
-          <Route exact path="/signUp" component={SignUp} />
+          {/* <Route exact path="/signUp" component={SignUp} /> */}
           <Route exact path="/login" component={Login} />
           <Route exact path="/" component={Main} />
           <Route exact path="/product-detail" component={ProductDetail} />
