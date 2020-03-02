a308c7845388089f452deb57d259a1e4c039bb1c
sunghaeJoung <shh03288@gmail.com>
2020-03-02 16:00:40 +0900

    오류수정

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import SignUp from './Pages/SignUp/SignUp';
+// import SignUp from './Pages/SignUp/SignUp';
 import Login from './Pages/Login/Login';
 import Main from './Pages/Main/Main.js';
 import ListDetail from './Pages/ListDetail/ListDetail.js';
@@ -12,7 +12,7 @@ class Routes extends React.Component {
       <Router>
         <Switch>
           <Route exact path="/" component={Main} />
-          <Route exact path="/signUp" component={SignUp} />
+          {/* <Route exact path="/signUp" component={SignUp} /> */}
           <Route exact path="/login" component={Login} />
           <Route exact path="/listDetail" component={ListDetail} />
           <Route exact path="/search" component={Search} />
